c334ea863405705ebdaf0cabe2def0a5d5dda593
Pablo Lizio <pablo.lizio@gmail.com>
2021-02-28 00:56:39 +0100

    -Implementacion del servicio de autentificacion con comunicacion real con (https://reqres.in/ API) fake backend -Implementacion de servicio de local storage para persistencia de la session

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,9 +1,22 @@
+import { User } from './../../models/user';
+import { Observable } from 'rxjs';
+import { LoginUser } from './../../models/login-user';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  private basePath = 'https://reqres.in/api/';
+
+  login(loginUser: LoginUser): Observable<any> {
+    return this.http.post(this.basePath + 'login', loginUser);
+  }
+
+  sigup(newUser: User): Observable<any> {
+    return this.http.post(this.basePath + 'register', newUser);
+  }
 }
--- src/app/services/storage/storage.service.ts
+++ src/app/services/storage/storage.service.ts
@@ -1,9 +1,38 @@
+import { Router } from '@angular/router';
+import { SessionToken } from './../../models/session-token';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageService {
+  private currentSession: SessionToken = null;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.currentSession = this.loadSessionData();
+  }
+
+  setCurrentSession(session: SessionToken): void {
+    this.currentSession = session;
+    localStorage.setItem('currentUser', JSON.stringify(session));
+  }
+
+  loadSessionData(): SessionToken {
+    const savedSession = localStorage.getItem('currentUser');
+    return savedSession ? <SessionToken>JSON.parse(savedSession) : null;
+  }
+
+  removeCurrentSession(): void {
+    localStorage.removeItem('currentUser');
+    this.currentSession = null;
+  }
+
+  isAuthenticated(): boolean {
+    return this.currentSession.token != null ? true : false;
+  }
+
+  logout(): void {
+    this.removeCurrentSession();
+    this.router.navigate(['/login']);
+  }
 }
