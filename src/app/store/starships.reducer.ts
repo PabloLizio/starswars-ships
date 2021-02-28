d7bb3f92e976179e900c3c43a262a8c94027c9c0
Pablo Lizio <pablo.lizio@gmail.com>
2021-02-28 17:30:45 +0100

    Se creo el reducer el cual responde a las dos acciones creadas y le asigna el estado incial de la store

---
+++ src/app/store/starships.reducer.ts
@@ -0,0 +1,23 @@
+import { Starship } from 'src/app/models/starship';
+import { loadNewPage, pageLoaded } from './starships.actions';
+import { Action, createReducer, on } from '@ngrx/store';
+
+export const initialState: Starship[] = [
+  {
+    name: '',
+    url: '',
+  },
+];
+
+const _starshipsReducer = createReducer(
+  initialState,
+  on(loadNewPage, (state) => state),
+  on(pageLoaded, (state, { ships }) => ships)
+);
+
+export function starshipsReducer(
+  state: Starship[] | undefined = initialState,
+  action: Action
+) {
+  return _starshipsReducer(state, action);
+}
