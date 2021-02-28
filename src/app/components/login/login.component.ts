4c8215c6b7cd9cc46c0be70418a2b89c90452a64
Pablo Lizio <pablo.lizio@gmail.com>
2021-02-28 01:01:13 +0100

    - Implementacion del Login form y conecciopn con el servicio de autentificacion
    Resuelve:
    - Login no realiza ningun llamado al servidor API para checkar el usario y constraseña
    - El registro del usuario no persiste al recargar la App

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,9 @@
+import { StorageService } from './../../services/storage/storage.service';
+import { SessionToken } from './../../models/session-token';
+import { AuthenticationService } from './../../services/authentication/authentication.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
-
-// JSON
-import usersList from 'src/assets/json/users.json';
 
 @Component({
   selector: 'app-login',
@@ -13,11 +13,14 @@ import usersList from 'src/assets/json/users.json';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   dataLoading: boolean = false;
-  users: any = usersList;
-  unregistered: boolean = false;
-  invalid: boolean = false;
+  loginError: boolean = false;
 
-  constructor(private fb: FormBuilder, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private auth: AuthenticationService,
+    private storage: StorageService
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
@@ -28,20 +31,28 @@ export class LoginComponent implements OnInit {
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
-  loginUser() {
-    if (this.loginForm.invalid) {
-      return;
+
+  loginUser(): void {
+    this.dataLoading = true;
+
+    if (this.loginForm.valid) {
+      this.auth.login(this.loginForm.value).subscribe(
+        (data) => this.loginSuccess(data),
+        (error) => this.loginFailed(error)
+      );
     }
-    // TODO : Falta integrar el servicio para autentificar al usuario
-    // JSON simulando usuarios
-    var userLogin = this.loginForm.value.username;
-    var filterJson = this.users.filter(function (user) {
-      return user.first_name === userLogin;
-    });
-    if (filterJson.length > 0) {
-      this.router.navigate(['/principal/ships']);
-    } else {
-      this.unregistered = true;
-    }
+  }
+
+  private loginSuccess(data: SessionToken) {
+    console.log('succes');
+    this.dataLoading = false;
+    this.storage.setCurrentSession(data);
+    this.router.navigate(['/principal/ships']);
+  }
+
+  private loginFailed(error) {
+    this.dataLoading = false;
+    this.loginError = true;
+    console.log(error);
   }
 }
