92d0b88e138e9c7c62f5332421e32b1ddd045a63
Pablo Lizio <pablo.lizio@gmail.com>
2021-02-28 17:29:22 +0100

    Se creo el effect de la Starships Store para la accion de loadNewPage y ejecuta la accion pageLoaded en respuesta

---
+++ src/app/store/starships.effects.ts
@@ -0,0 +1,21 @@
+import { ShipsService } from './../services/ships/ships.service';
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { map, mergeMap } from 'rxjs/operators';
+import { loadNewPage, pageLoaded } from './starships.actions';
+
+@Injectable()
+export class StarshipsEffects {
+  loadPage$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadNewPage),
+      mergeMap((action) => {
+        return this.shipsService
+          .getShips(action.page)
+          .pipe(map((starships) => pageLoaded({ ships: starships.results })));
+      })
+    )
+  );
+
+  constructor(private actions$: Actions, private shipsService: ShipsService) {}
+}
