1246186e961a7b59e2bb1fc42f41f61810e71138
Pablo Lizio <pablo.lizio@gmail.com>
2021-03-01 22:20:55 +0100

    fix: Se corrigio el path de redireccion la path correcto

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -8,7 +8,7 @@ import { RegisterComponent } from '../../components/register/register.component'
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '**', redirectTo: 'principal/ships', pathMatch: 'full' },
+  { path: '', redirectTo: '/principal/ships', pathMatch: 'full' },
 ];
 
 @NgModule({
