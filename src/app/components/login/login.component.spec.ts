3f90b990f6b5cf2777cb06251a1ccbb4fc8767fe
Pablo Lizio <pablo.lizio@gmail.com>
2021-03-01 01:27:26 +0100

    Se añadieron los Tests unitarios para los formularios de loscomponentes de Login y Registro

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -6,6 +6,19 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginComponent } from './login.component';
 
+// List of test data for the Login Form with its expected validity result
+const dataTestLoginForm = [
+  //[ email, password, expected ]
+  ['', '', false],
+  ['aa', 'aaaaa', false],
+  ['a@a', 'aa45aa', true],
+  ['aa', 'aaaaaa', false],
+  ['aaeert', '123456aa', false],
+  ['a@a', '12345', false],
+  ['ar@ar.co', '123def', true],
+  ['', '', false],
+];
+
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
@@ -34,5 +47,37 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form validity should be equal as asignated on the list for all the list', () => {
+    let emailInput = component.loginForm.controls['email'];
+    let passwordInput = component.loginForm.controls['password'];
+
+    let result = dataTestLoginForm.reduce(
+      (acc, [email, password, expected]) => {
+        emailInput.setValue(email);
+        passwordInput.setValue(password);
+        return component.loginForm.valid !== expected ? false : acc;
+      },
+      true
+    );
+    expect(result).toBeTruthy();
+  });
+
+  it('button should be disabled', () => {
+    let button = document.querySelector('.buttons__login');
+    expect(button.hasAttribute('disabled')).toBeTruthy();
+  });
+
+  it('button should not be disabled', () => {
+    component.loginForm.controls['email'].setValue('ab@cd');
+    component.loginForm.controls['password'].setValue('123456');
+
+    setTimeout(() => {
+      let buttonState = document
+        .querySelector('.buttons__login')
+        .hasAttribute('disabled');
+      expect(buttonState).toBeFalsy();
+    }, 10);
   });
 });
--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -4,6 +4,21 @@ import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { RegisterComponent } from './register.component';
 
+const dataTestregisterForm = [
+  ['', '', '', '', false],
+  ['abc', 'abc', 'a@b', 'abcdef', true],
+  ['abc', 'abc', 'a@b', 'abcde', false],
+  ['abc', 'abc', 'a@', 'abcdef', false],
+  ['abc', 'ab', 'a@b', 'abcdef', false],
+  ['ab', 'abc', 'a@b', 'abcdef', false],
+  ['abc', 'abc', 'a@b.c', 'abcdef', true],
+  ['abc', 'abc', 'a@b.c', '123456', true],
+  ['abcqwe', 'abcqwe', 'a@b.c', '123456', true],
+  ['', '', 'a@b', '123456', false],
+  ['', 'qwerwq', 'a@b', '123456', false],
+  ['werwer', '', 'a@b', '123456', false],
+];
+
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
@@ -30,5 +45,42 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form validity should be equal as asignated on the list for all the list', () => {
+    let emailInput = component.registerForm.controls['email'];
+    let passwordInput = component.registerForm.controls['password'];
+    let first_nameInput = component.registerForm.controls['first_name'];
+    let last_nameInput = component.registerForm.controls['last_name'];
+    let result = dataTestregisterForm.reduce(
+      (acc, [first_name, last_name, email, password, expected]) => {
+        emailInput.setValue(email);
+        passwordInput.setValue(password);
+        first_nameInput.setValue(first_name);
+        last_nameInput.setValue(last_name);
+        return component.registerForm.valid !== expected ? false : acc;
+      },
+      true
+    );
+    expect(result).toBeTruthy();
+  });
+
+  it('button should be disabled', () => {
+    let button = document.querySelector('.buttons__login');
+    expect(button.hasAttribute('disabled')).toBeTruthy();
+  });
+
+  it('button should not be disabled', () => {
+    component.registerForm.controls['email'].setValue('ab@cd');
+    component.registerForm.controls['password'].setValue('123456');
+    component.registerForm.controls['first_name'].setValue('abc');
+    component.registerForm.controls['last_name'].setValue('abc');
+
+    setTimeout(() => {
+      let buttonState = document
+        .querySelector('.buttons__login')
+        .hasAttribute('disabled');
+      expect(buttonState).toBeFalsy();
+    }, 10);
   });
 });
